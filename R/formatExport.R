8acb782da26976838c6a285dfef30b3ab7e1f876
wilsontom <wilsontom89@gmail.com>
2019-10-03 19:11:08 +0100

    add format function

---
+++ R/formatExport.R
@@ -0,0 +1,56 @@
+#' Format Recruitment Export
+#'
+#' The recruitment database is populated using a Jisc survey which is e-mailed out to potential contacts. This function
+#' formats the direct export from Jisc ready for validation and upload using `waruDB`
+#'
+#' @param x a `tibble` of the raw Jisc output using `readr::read_csv`
+#' @return a `tibble`
+#' @export
+
+formatExport <- function(x)
+{
+  RENAME <- c(
+    'DATE',
+    'TITLE',
+    'Forename',
+    'Surname',
+    'DOB',
+    'ContactNumber',
+    'EmailAddress',
+    'PostCode',
+    'ContactMethod',
+    'Smoker',
+    'Allergens',
+    'WhichAllergens',
+    'DietReq',
+    'BloodSample',
+    'BloodSampleComment',
+    'UrineSample',
+    'UrineSampleComment'
+  )
+
+  names(x) <- RENAME
+
+  x$DATE <- lubridate::dmy(x$DATE)
+  x$DOB <- lubridate::dmy(x$DOB)
+
+  x$Smoker[x$Smoker == 'No'] <- FALSE
+  x$Smoker[x$Smoker == 'Yes'] <- TRUE
+
+  x$Allergens[x$Allergens == 'No'] <- FALSE
+  x$Allergens[x$Allergens == 'Yes'] <- TRUE
+
+  x$UrineSample[x$UrineSample == 'No'] <- FALSE
+  x$UrineSample[x$UrineSample == 'Yes'] <- TRUE
+
+  x$BloodSample[x$BloodSample == 'No'] <- FALSE
+  x$BloodSample[x$BloodSample == 'Yes'] <- TRUE
+
+  x$PostCode <- toupper(x$PostCode)
+  x$PostCode <- gsub(' ', '', x$PostCode)
+
+  x$ContactNumber <- gsub(' ', '', x$ContactNumber)
+
+  return(x)
+
+}
